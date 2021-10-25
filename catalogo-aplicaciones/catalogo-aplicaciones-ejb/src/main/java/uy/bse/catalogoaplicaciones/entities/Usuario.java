1a83b6c4c7c4d00946e7876c1ceb762cae198abe
lucanales <lucanales@gmail.com>
2021-10-25 16:53:58 -0300

    Se agrega al pom la configuración para jpa

--- catalogo-aplicaciones/catalogo-aplicaciones-ejb/src/main/java/uy/bse/catalogoaplicaciones/entities/Usuario.java
+++ catalogo-aplicaciones/catalogo-aplicaciones-ejb/src/main/java/uy/bse/catalogoaplicaciones/entities/Usuario.java
@@ -1,5 +1,53 @@
 package uy.bse.catalogoaplicaciones.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String documentoIdentidad;
+	private String email;
+	
+	
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getDocumentoIdentidad() {
+		return documentoIdentidad;
+	}
+	public void setDocumentoIdentidad(String documentoIdentidad) {
+		this.documentoIdentidad = documentoIdentidad;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
