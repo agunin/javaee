eab93cb4956d3290b0be3cfe2e32836d22e09ff5
agunin <aguquimnin@gmail.com>
2021-11-18 16:50:05 -0300

    subo relacion de ambiente en jpa

--- catalogo-aplicaciones/catalogo-aplicaciones-entity/src/main/java/uy/bse/catalogoaplicaciones/domain/Ambiente.java
+++ catalogo-aplicaciones/catalogo-aplicaciones-entity/src/main/java/uy/bse/catalogoaplicaciones/domain/Ambiente.java
@@ -4,9 +4,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -16,7 +19,7 @@ public class Ambiente extends BaseEntity<Long> {
 
 	@Id
 	@Column(name = "idAmbiente")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "ambienteTipo", nullable = false)
@@ -30,9 +33,31 @@ public class Ambiente extends BaseEntity<Long> {
 	@NotNull
 	private String url;
 
+	
+	//ManytoOne a Aplicacion
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="fk_aplicacion")
+	private Aplicacion aplicacion;
+
+	//ManytoOne a SolInfra
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="fk_solinfra")
+	private SolInfra solInfra;
+	
 	public Ambiente() {
 		super();
 	}
+	
+	
+
+	public Ambiente( AmbienteTipo ambienteTipo, Integer puerto,  String url) {
+		super();
+		this.ambienteTipo = ambienteTipo;
+		this.puerto = puerto;
+		this.url = url;
+	}
+
+
 
 	public Long getId() {
 		return id;
@@ -64,6 +89,22 @@ public class Ambiente extends BaseEntity<Long> {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+	
+	public Aplicacion getAplicacion() {
+		return aplicacion;
+	}
+
+	public void setAplicacion(Aplicacion aplicacion) {
+		this.aplicacion = aplicacion;
+	}
+
+	public SolInfra getSolInfra() {
+		return solInfra;
+	}
+
+	public void setSolInfra(SolInfra solInfra) {
+		this.solInfra = solInfra;
 	}
 
 }
