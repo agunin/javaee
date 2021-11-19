49e4e67b35e2d1cd62006eee3fd942cc0c2dc22b
lucanales <lucanales@gmail.com>
2021-11-19 11:42:58 -0300

    datos en cargadb

--- catalogo-aplicaciones/catalogo-aplicaciones-ejb/src/main/java/uy/bse/catalogoaplicaciones/ejbs/CargaDBEJB.java
+++ catalogo-aplicaciones/catalogo-aplicaciones-ejb/src/main/java/uy/bse/catalogoaplicaciones/ejbs/CargaDBEJB.java
@@ -27,136 +27,193 @@ public class CargaDBEJB {
 		
 		/************************************************** Usuarios ****************************************************/
 		
-		Usuario u1 = new Usuario("Agustin", "Nin", "41059731","aguquimnin@gmail.com");
+		Usuario u1 = new Usuario("Agustin", "Nin", "4.105.973-1","aguquimnin@gmail.com");
 		em.persist(u1);
 		em.flush();
 		
-		Usuario u2 = new Usuario("Luciana", "Canales", "40000001","lcanales@gmail.com");
+		Usuario u2 = new Usuario("Luciana", "Canales", "3.234.675-9","lucanales@gmail.com");
 		em.persist(u2);
+		em.flush();
+		
+		Usuario u3 = new Usuario("Laura", "Rodriguez", "3.111.575-3","laurodriguez@gmail.com");
+		em.persist(u3);
+		em.flush();
+		Usuario u4 = new Usuario("Pablo", "Perez", "2.311.575-1","pabloperez@gmail.com");
+		em.persist(u4);
 		em.flush();
 		
 		/****************************************************************************************************************/
 		/************************************************ Interfaces ****************************************************/
-		Interface inter = new Interface("InterfaceCubos", "Interface para publicar cubos", InterfaceTipo.PROCEDURE_SQL);
+		Interface inter = new Interface("ICubos", "Interface para publicar cubos.", InterfaceTipo.PROCEDURE_SQL);
 		em.persist(inter);
 		em.flush();
-
-		Interface inter1 = new Interface("Interface1", "Esta es la Interface1", InterfaceTipo.PROCEDURE_SQL);
-		em.persist(inter1);
-		em.flush();
-
-		Interface inter2 = new Interface("Interface2", "Esta es la Interface2", InterfaceTipo.PROCEDURE_SQL);
-		em.persist(inter2);
+		Rol rol_inter = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter.setUsuario(u1);
+		rol_inter.setComponenteSoftware(inter);
+		em.persist(rol_inter);
 		em.flush();
 		
-		Interface inter3 = new Interface("Interface3", "Esta es la Interface3", InterfaceTipo.BATCH);
+		Interface inter1 = new Interface("ICargaHoras", "Interface para la carga de hora de funcionarios.", InterfaceTipo.REST);
+		em.persist(inter1);
+		em.flush();
+		Rol rol_inter1 = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter1.setUsuario(u1);
+		rol_inter1.setComponenteSoftware(inter1);
+		em.persist(rol_inter);
+		em.flush();
+		
+		Interface inter2 = new Interface("IFacturacionInterna", "Interface para el calculo de facturación interna de la empresa.", InterfaceTipo.PROCEDURE_SQL);
+		em.persist(inter2);
+		em.flush();
+		Rol rol_inter2 = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter2.setUsuario(u2);
+		rol_inter2.setComponenteSoftware(inter2);
+		em.persist(rol_inter2);
+		em.flush();
+		
+		Interface inter3 = new Interface("IDatalogicGIA", "Interface externa provista por Datalogic para la gestión de rrhh.", InterfaceTipo.SOAP);
 		em.persist(inter3);
 		em.flush();
-		Interface inter4 = new Interface("Interface4", "Esta es la Interface4", InterfaceTipo.REST);
+		Interface inter4 = new Interface("IDatalogicDGI", "Interface externa provista por Datalogic para la gestión con dgi.", InterfaceTipo.SOAP);
 		em.persist(inter4);
 		em.flush();
-		Interface inter5 = new Interface("Interface5", "Esta es la Interface5", InterfaceTipo.PROCEDURE_SQL);
-		em.persist(inter5);
-		em.flush();
-		Interface inter6 = new Interface("Interface6", "Esta es la Interface6", InterfaceTipo.SOAP);
+		Interface inter6 = new Interface("IBMP", "Interface externa provista por IBM.", InterfaceTipo.SOAP);
 		em.persist(inter6);
 		em.flush();
-		Interface inter7 = new Interface("Interface7", "Esta es la Interface7", InterfaceTipo.PROCEDURE_SQL);
+		
+		Interface inter5 = new Interface("ISiniestros", "Interface que provee los métodos necesarios para el proceso del siniestro.", InterfaceTipo.REST);
+		em.persist(inter5);
+		em.flush();
+		Rol rol_inter5 = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter5.setUsuario(u2);
+		rol_inter5.setComponenteSoftware(inter5);
+		em.persist(rol_inter5);
+		em.flush();
+		
+		Interface inter7 = new Interface("IPresentismo", "Interface para la gestión de licencias de funcionarios.", InterfaceTipo.REST);
 		em.persist(inter7);
 		em.flush();
-		Interface inter8 = new Interface("Interface8", "Esta es la Interface8", InterfaceTipo.BATCH);
+		Rol rol_inter7 = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter7.setUsuario(u2);
+		rol_inter7.setComponenteSoftware(inter7);
+		em.persist(rol_inter7);
+		em.flush();
+		
+		Interface inter8 = new Interface("ISueldos", "Interface para la gestión de sueldos de funcionarios", InterfaceTipo.SOAP);
 		em.persist(inter8);
 		em.flush();
-		Interface inter9 = new Interface("Interface9", "Esta es la Interface9", InterfaceTipo.BATCH);
-		em.persist(inter9);
-		em.flush();
-		Interface inter10 = new Interface("Interface10", "Esta es la Interface10", InterfaceTipo.REST);
-		em.persist(inter10);
+		Rol rol_inter8 = new Rol(RolTipo.DESARROLLADOR);
+		rol_inter8.setUsuario(u2);
+		rol_inter8.setComponenteSoftware(inter8);
+		em.persist(rol_inter8);
 		em.flush();
 
 		/****************************************************************************************************************/
 		/************************************************ Aplicaciones **************************************************/
 		
 		
-		Aplicacion app1 = new Aplicacion("ps", "Pentaho Server : Plataforma web para integracion de suite Pentaho", AplicacionLenguaje.PENTAHO);
+		Aplicacion app1 = new Aplicacion("AppPentahoServer", "Aplicacion web para integracion de suite Pentaho.", AplicacionLenguaje.PENTAHO);
 		em.persist(app1);
 		em.flush();
-
+		// interfaces que provee
 		app1.addProveeInterface(inter);
+		app1.addProveeInterface(inter6);
 		em.persist(app1);
 		em.flush();
-		
-		app1.addConsumeInterface(inter);
-		em.persist(app1);
+		Rol rol_app1 = new Rol(RolTipo.DESARROLLADOR);
+		rol_app1.setUsuario(u1);
+		rol_app1.setComponenteSoftware(app1);
+		em.persist(rol_app1);
 		em.flush();
 		
-
-		Aplicacion app2 = new Aplicacion("pdi", "Pentaho Data Integration : Aplicacion para desarrollar ETLs", AplicacionLenguaje.PENTAHO);
+		Aplicacion app2 = new Aplicacion("ApppPentahoDataIntegration", "Aplicacion para desarrollar ETLs.", AplicacionLenguaje.PENTAHO);
 		em.persist(app2);
 		em.flush();
-
-		app2.addProveeInterface(inter1);
-		app2.addProveeInterface(inter2);
-		app2.addProveeInterface(inter3);
-		app2.addProveeInterface(inter4);
-		app2.addProveeInterface(inter5);
-		app2.addProveeInterface(inter6);
-		app2.addProveeInterface(inter7);
-		app2.addProveeInterface(inter8);
-		app2.addProveeInterface(inter9);
-		app2.addProveeInterface(inter10);
-		em.persist(app2);
-		em.flush();
-		
-		app2.addConsumeInterface(inter1);
-		app2.addConsumeInterface(inter2);
-		app2.addConsumeInterface(inter3);
-		app2.addConsumeInterface(inter4);
-		app2.addConsumeInterface(inter5);
+		// interfaces que consume
 		app2.addConsumeInterface(inter6);
-		app2.addConsumeInterface(inter7);
-		app2.addConsumeInterface(inter8);
-		app2.addConsumeInterface(inter9);
-		app2.addConsumeInterface(inter10);
 		em.persist(app2);
 		em.flush();
-
-		app2.addProveeInterface(inter2);
-		em.persist(app2);
+		Rol rol_app2 = new Rol(RolTipo.DESARROLLADOR);
+		rol_app2.setUsuario(u1);
+		rol_app2.setComponenteSoftware(app2);
+		em.persist(rol_app2);
 		em.flush();
-
-		Aplicacion app3 = new Aplicacion("psw", "Pentaho Schema Workbench : Aplicacion para desarrollar Cubos", AplicacionLenguaje.PENTAHO);
+		
+		Aplicacion app3 = new Aplicacion("ApppPentahoSchemaWorkbench", "Aplicacion para desarrollar Cubos.", AplicacionLenguaje.PENTAHO);
 		em.persist(app3);
 		em.flush();
+		// interfaces que consume
+		app3.addConsumeInterface(inter6);
+		em.persist(app3);
+		em.flush();
+		Rol rol_app3 = new Rol(RolTipo.DESARROLLADOR);
+		rol_app3.setUsuario(u1);
+		rol_app3.setComponenteSoftware(app3);
+		em.persist(rol_app3);
+		em.flush();
 
-		Aplicacion app4 = new Aplicacion("Aplicacion4", "Esta es la Aplicacion4", AplicacionLenguaje.JAVA);
+		Aplicacion app4 = new Aplicacion("AppRRHH", "Aplicacion para la gestion de rrhh.", AplicacionLenguaje.JAVA);
 		em.persist(app4);
 		em.flush();
+		// interfaces que provee
+		app4.addProveeInterface(inter1);
+		app4.addProveeInterface(inter7);
+		app4.addProveeInterface(inter8);
+		em.persist(app4);
+		em.flush();
+		// interfaces que consume
+		app4.addConsumeInterface(inter3);
+		em.persist(app4);
+		em.flush();
+		Rol rol_app4 = new Rol(RolTipo.DESARROLLADOR);
+		rol_app4.setUsuario(u2);
+		rol_app4.setComponenteSoftware(app4);
+		em.persist(rol_app4);
+		em.flush();
+		Rol rolFun_app4 = new Rol(RolTipo.OPERADOR_FUNCIONAL);
+		rolFun_app4.setUsuario(u3);
+		rolFun_app4.setComponenteSoftware(app4);
+		em.persist(rolFun_app4);
+		em.flush();
 
-		Aplicacion app5 = new Aplicacion("Aplicacion5", "Esta es la Aplicacion5", AplicacionLenguaje.JAVA);
+		Aplicacion app5 = new Aplicacion("AppFacturacion", "Aplicacion para la gestion de facturación de seguros.", AplicacionLenguaje.JAVA);
 		em.persist(app5);
 		em.flush();
-
-		Aplicacion app6 = new Aplicacion("Aplicacion6", "Esta es la Aplicacion6", AplicacionLenguaje.PHP);
-		em.persist(app6);
+		// interfaces que provee
+		app5.addProveeInterface(inter2);
+		app5.addProveeInterface(inter5);
+		em.persist(app5);
 		em.flush();
-
-		Aplicacion app7 = new Aplicacion("Aplicacion7", "Esta es la Aplicacion7", AplicacionLenguaje.JAVA);
-		em.persist(app7);
+		// interfaces que consume
+		app5.addConsumeInterface(inter4);
+		em.persist(app5);
 		em.flush();
-
-		Aplicacion app8 = new Aplicacion("Aplicacion8", "Esta es la Aplicacion8", AplicacionLenguaje.APEX);
-		em.persist(app8);
+		Rol rol_app5 = new Rol(RolTipo.DESARROLLADOR);
+		rol_app5.setUsuario(u2);
+		rol_app5.setComponenteSoftware(app5);
+		em.persist(rol_app5);
 		em.flush();
-
-		Aplicacion app9 = new Aplicacion("Aplicacion9", "Esta es la Aplicacion9", AplicacionLenguaje.JAVA);
-		em.persist(app9);
+		Rol rolFun_app5 = new Rol(RolTipo.OPERADOR_FUNCIONAL);
+		rolFun_app5.setUsuario(u4);
+		rolFun_app5.setComponenteSoftware(app5);
+		em.persist(rolFun_app5);
 		em.flush();
-
-		Aplicacion app10 = new Aplicacion("Aplicacion10", "Esta es la Aplicacion10", AplicacionLenguaje.JAVA);
-		em.persist(app10);
+		
+		/****************************************************************************************************************/
+		/************************************************** Solucion ****************************************************/	
+		Solucion sol = new Solucion("SolCentralBanco", "Solucion core para gestion de cada accion del banco.");
+		em.persist(sol);
 		em.flush();
-
+		sol.addComponentesSoftware(app1);
+		sol.addComponentesSoftware(app2);
+		sol.addComponentesSoftware(app3);
+		sol.addComponentesSoftware(app4);
+		sol.addComponentesSoftware(app5);
+		sol.addComponentesSoftware(inter7);
+		sol.addComponentesSoftware(inter8);
+		sol.addComponentesSoftware(inter5);
+		em.persist(sol);
+		em.flush();
 		/****************************************************************************************************************/
 		/************************************************** SolInfra ****************************************************/	
 		
@@ -201,34 +258,6 @@ public class CargaDBEJB {
 		ambiente01.setSolInfra(servidor1);
 		em.persist(ambiente01);
 		em.flush();
-		
-		/****************************************************************************************************************/
-		/****************************************************** Rol *****************************************************/	
-		
-		Rol u1_desarrollador_app1 = new Rol(RolTipo.DESARROLLADOR);
-		u1_desarrollador_app1.setUsuario(u1);
-		u1_desarrollador_app1.setComponenteSoftware(app1);
-		//desarrollador.addComponenteSoftware(app2);
-		em.persist(u1_desarrollador_app1);
-		em.flush();
-		
-	
-		/*Rol u2_desarrollador_app1 = new Rol(RolTipo.DESARROLLADOR);
-		u2_desarrollador_app1.setUsuario(u2);
-		u2_desarrollador_app1.setComponenteSoftware(app1);
-		//desarrollador.addComponenteSoftware(app2);
-		em.persist(u2_desarrollador_app1);
-		em.flush();
-		
-		Rol u1_funcional_app1 = new Rol(RolTipo.OPERADOR_FUNCIONAL);
-		u1_funcional_app1.setUsuario(u1);
-		u1_funcional_app1.setComponenteSoftware(app2);
-		em.persist(u1_funcional_app1);
-		em.flush();*/
-		
-		/****************************************************************************************************************/
-		
-
 	}
 
 	@PreDestroy
--- catalogo-aplicaciones/catalogo-aplicaciones-entity/src/main/java/uy/bse/catalogoaplicaciones/domain/ComponenteSoftware.java
+++ catalogo-aplicaciones/catalogo-aplicaciones-entity/src/main/java/uy/bse/catalogoaplicaciones/domain/ComponenteSoftware.java
@@ -1,14 +1,7 @@
 package uy.bse.catalogoaplicaciones.domain;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -17,34 +10,28 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-//@Table(name = "Componente_Software") por defecto le pone de nombre ComponenteSoftware
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class ComponenteSoftware extends BaseEntity<Long> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-
 	@NotNull
-	@Column(unique=true)
+	@Column(unique = true)
 	private String identificador;
 
 	@Lob
 	@NotNull
-	@Size(min = 4)
+	@Size(min = 30)
 	@Basic(fetch = FetchType.LAZY)
 	private String descripcion;
-	
 
 	public Long getId() {
 		return id;
@@ -53,7 +40,7 @@ public abstract class ComponenteSoftware extends BaseEntity<Long> {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getIdentificador() {
 		return identificador;
 	}
@@ -69,7 +56,5 @@ public abstract class ComponenteSoftware extends BaseEntity<Long> {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	
 
 }
